548627be2e05e7b68e5465d563d45fe51ef99005
snamiki1212 <snamiki1212@users.noreply.github.com>
2021-08-09 09:09:31 +0000

    deploy: 7ea19edc3eef338eb2935d2442c58936c41f9e76

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-2a407f63bb0c20085df9.js"
+    "url": "webpack-runtime-972f44d387bd01dca217.js"
   },
   {
-    "url": "styles.4fa3efbaa7d69f4b068e.css"
+    "url": "styles.f6e2031abd45635bbb50.css"
   },
   {
     "url": "framework-c10f6eb6e8543569985b.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f862aef908033a7df7ff1a4ea3475e2e"
+    "revision": "68df8cde0c1303d5c376ae91beb66560"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-fc7e9a128b06f763b6e5.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ad5022571d98ff4fce2f834c07b4ae07"
+    "revision": "3cb8b48369474cc5f18cc99b43b35aaf"
   },
   {
     "url": "polyfill-d33c729d549a3ce33e65.js"
